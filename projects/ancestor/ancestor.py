50cbc9ecace90e205170208d63a804ba2e526b73
Robert Driskell <driskell.ba@gmail.com>
2019-10-08 15:04:16 -0500

    Structuring family tree in to graph-like structure

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,14 @@
+class Vertex:
+    def __init__(self, id, parents):
+        self.parents = parents
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    vertexes = {}
+    for i in ancestors:
+        if i[1] in vertexes:
+            vertexes[i[1]].parents.append(i[0])
+        else:
+            vertexes[i[1]] = Vertex(i[1], [i[0]])
+    
+    
