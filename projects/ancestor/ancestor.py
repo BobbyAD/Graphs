7b6ebb3d43866b6bbe5efa65edfb92d7ef3e05de
Robert Driskell <driskell.ba@gmail.com>
2019-10-08 15:17:23 -0500

    Got it done with recursion

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,6 +4,18 @@ class Vertex:
 
 
 def earliest_ancestor(ancestors, starting_node):
+    def recursive_find(vertexes, starting_node, path = []):
+        path.append(starting_node)
+        if starting_node in vertexes:
+            new_list = []
+            for i in vertexes[starting_node].parents:
+                returned_list = recursive_find(vertexes, i, path.copy())
+                if len(returned_list) > len(new_list):
+                    new_list = returned_list
+            return new_list
+        else:
+            return path
+    #One way paths going up family tree
     vertexes = {}
     for i in ancestors:
         if i[1] in vertexes:
@@ -11,4 +23,12 @@ def earliest_ancestor(ancestors, starting_node):
         else:
             vertexes[i[1]] = Vertex(i[1], [i[0]])
     
+    if starting_node not in vertexes:
+        return -1
+    
+    longest_branch = recursive_find(vertexes, starting_node)
+
+    print(longest_branch)
+
+    return longest_branch[-1]
     
