7999eadc224378671f30e6c5690374657c4ab7bf
Robert Driskell <driskell.ba@gmail.com>
2019-10-09 16:10:52 -0500

    I already completed stretch because I didn't read the hints like a cool guy. Added check for avg degrees of seperation

--- projects/social/social.py
+++ projects/social/social.py
@@ -66,6 +66,8 @@ class SocialGraph:
         for i in range(0, numUsers):
             self.addUser(f"{i}")
 
+        calls = 0
+
         # Create friendships
         for u in self.users:
             for f in range(0, random.randint(0, avgFriendships)):
@@ -74,6 +76,10 @@ class SocialGraph:
                 while friend in self.friendships[u] or friend is u:
                     friend = random.randint(1,numUsers)
                 self.addFriendship(u, friend)
+                calls += 1
+
+        print("Calls to addFriendship")
+        print(calls)
 
     def getAllSocialPaths(self, userID):
         """
@@ -91,10 +97,13 @@ class SocialGraph:
 
         q.enqueue([userID])
 
+        #BFT
         while q.size() > 0:
             path = q.dequeue()
             vertex = path[-1]
             if vertex in visited:
+                #This doesn't ever actually execute because BFT hits shortest path first
+                #Good boilerplate for swapping to see longest path, though
                 if len(path) < len(visited[vertex]):
                     visited[vertex] = path.copy()
             else:
@@ -109,7 +118,7 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populateGraph(10, 2)
+    sg.populateGraph(1000, 5)
     print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
@@ -117,9 +126,44 @@ if __name__ == '__main__':
     """
     Sanity check for averages
     """
-    # total = 0
-    # for i in range(1, len(sg.friendships)+1):
-    #     total += len(sg.friendships[i])
+    total = 0
+    for i in range(1, len(sg.friendships)+1):
+        total += len(sg.friendships[i])
 
-    # total /= len(sg.friendships)
-    # print(total)
+    total /= len(sg.friendships)
+    print("Average Friendships")
+    print(total)
+
+    """
+    Average degrees of seperation
+    """
+    total = 0
+    avg_sep = 0
+    for i in range(1, len(sg.friendships)+1):
+        con = sg.getAllSocialPaths(i)
+        total += len(con)
+
+        cur_sep_avg = 0
+        for i in con:
+            cur_sep_avg += len(con[i])
+
+        if cur_sep_avg > 0:
+            cur_sep_avg /= len(con)
+        avg_sep += cur_sep_avg
+
+
+    total /= len(sg.friendships)
+    print("Average Connections")
+    print(total)
+
+    avg_sep /= len(sg.friendships)
+    print("Average Degrees of Seperation")
+    print(avg_sep)
+
+
+"""
+    To create 100 users with an average of 10 friends each, how many times would you need to call addFriendship()? Why?
+        500ish - As I grow the list of connections, I don't have to make as many towards the end of the list where they're already connected.
+    If you create 1000 users with an average of 5 random friends each, what percentage of other users will be in a particular user's extended social network? What is the average degree of separation between a user and those in his/her extended network?
+        Almost every user (97ish percent) will be in their friend network with an average degree of seperation of 5. 
+"""
